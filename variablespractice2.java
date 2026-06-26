62211a591803bb09880210f78730c59d69a92d00
vinodext <vinoddakarpu@gmail.com>
2026-06-26 13:31:24 +0530

    added profile variables practice

---
+++ variablespractice2.java
@@ -0,0 +1,12 @@
+public class variablespractice2{
+	public static void main(String[]args){
+		String gamename="carxstreet";
+		int accountlevel=46;
+		double upgradecost=325025.132;
+		System.out.println("---profile status---");
+		System.out.println("Game:" +gamename);
+		System.out.println("Current level:" +accountlevel);
+		System.out.println("Car upgrade cost:$" +upgradecost);
+	
+	}
+}
